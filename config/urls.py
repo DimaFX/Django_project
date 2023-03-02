71fe5f49fee1904c07d298c8011363dea48b17ac
DimaFX <dm.mokrousoff@yandex.ru>
2023-03-02 23:35:40 +0300

    views_def

--- config/urls.py
+++ config/urls.py
@@ -14,13 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from mainapp import views
+from django.urls import path, include
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.hello),
-    path('blog/', views.blog),
-    path('<int:number>/', views.funk),
-    
+    path('', RedirectView.as_view(url='mainapp/')),
+    path('mainapp/', include('mainapp.urls')),     
+
 ]
+
